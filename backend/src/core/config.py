ee25c0c00b63928b1d1fc2614210d559945986e4
vighnesh hegde <vighneshhegde4@gmail.com>
2026-07-20 09:51:06 +0530

    initial commit

---
+++ backend/src/core/config.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+import dotenv
+
+dotenv.load_dotenv()
+
+class Config(BaseSettings):
+    EMBEDDING_API_KEY: str | None = None
+    OPENAI_API_KEY: str | None = None
+    AGENTIC_MODEL: str = "z-ai/glm-5.2"
+    GEMINI_API_KEY: str | None = None
+    QDRANT_URL: str = "http://qdrant:6333"
+    AI_API_BASE_URL: str = "https://integrate.api.nvidia.com/v1"
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+
+config = Config()
